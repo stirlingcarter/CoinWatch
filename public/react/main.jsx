7aed1ac378b833fdb31c0408bafde5f93ef74f1b
stirlingcarter <stirling.h.carter@vanderbilt.edu>
2017-09-02 17:24:16 -0500

    main

---
+++ public/react/main.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+export default class Main extends Component {
+  
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
