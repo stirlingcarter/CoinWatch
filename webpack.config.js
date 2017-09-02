263097487e9e374b694d468d74368d21ee85a3a6
Hunter Manhart <Hunter.M.Manhart@vanderbilt.edu>
2017-09-01 23:57:41 -0500

    Webpack + Yarn

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const webpack = require('webpack');
+
+// For extracting css from sass
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSASS = new ExtractTextPlugin('[name].css');
+
+module.exports = {
+    entry: {
+        
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'public/dist')
+    },
+    module: {      
+        rules: [
+            {
+                test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/,
+                loader: 'file-loader'
+            },
+            
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192
+                    }
+                }]
+            },
+            {
+                test: /\.scss$/,
+                use: extractSASS.extract({
+                    fallback: 'style-loader',
+                    //resolve-url-loader may be chained before sass-loader if necessary
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.(js|jsx)$/, 
+                exclude: /node_modules/, 
+                loader: "babel-loader",
+                query: {
+                    presets: ['es2015', 'react', 'stage-2']
+                } 
+            },            
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'jshint-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+        extractSASS,
+    ],
+    resolve: {
+        // alias: {
+        //     Classes: './public/javascripts/classes'
+        // },
+        extensions: [".jsx", ".js"]
+    },
+    watch: true
+};
