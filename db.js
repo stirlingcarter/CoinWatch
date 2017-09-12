8da0847c1623cc880ac19c4505835275ed1f38bc
stirlingcarter <stirling.h.carter@vanderbilt.edu>
2017-09-12 13:37:03 -0500

    forgot a file

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
-var knex = require('knex')({
+const knex = require('knex')({
     client: 'pg',
     connection: process.env.DATABASE_URL
   });
   
 var bookshelf = require('bookshelf')(knex);
 
+//require this given this
